34f972be270f6be50fb45e34c68044d772e6dd0d
Ali <iodk123@outlook.com>
2024-04-20 22:17:06 +0100

    started. got db working and some console functionality. still need to do add employee and update employee.

--- db/schema.sql
+++ db/schema.sql
@@ -1,34 +1,27 @@
--- Drop the database if it exists
 DROP DATABASE IF EXISTS employees;
-
--- Create the new database
 CREATE DATABASE employees;
 
--- Connect to the database
 \c employees
 
--- Create the department table
-CREATE TABLE department (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(30) UNIQUE NOT NULL
+CREATE TABLE departments (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
 );
 
--- Create the role table
-CREATE TABLE role (
-  id SERIAL PRIMARY KEY,
-  title VARCHAR(30) UNIQUE NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id INTEGER NOT NULL,
-  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
--- Create the employee table
-CREATE TABLE employee (
-  id SERIAL PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER NOT NULL,
-  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-  manager_id INTEGER,
-  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id) 
 );
